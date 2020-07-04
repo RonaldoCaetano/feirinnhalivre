e72d81c6f4401bfc74c47b9321ddcfaeb72b59bc
ronaldo.caetano <ronaldo.caetano@acct.global>
2020-07-03 23:07:37 -0300

    get user current location

--- src/pages/DashBoard/index.tsx
+++ src/pages/DashBoard/index.tsx
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, View, Text, Button } from 'react-native'
 import firebase from 'firebase'
+import * as Location from 'expo-location'
 
 export default function DashBoard() {
 	function signOutUser() {
 		firebase.auth().signOut()
 	}
 
+	const [currentLocation, setCurrentLocation] = useState<any>(null)
+
+	useEffect(() => {
+		;(async () => {
+			const { status } = await Location.requestPermissionsAsync()
+
+			if (status === 'granted') {
+				const location = await Location.getCurrentPositionAsync({})
+				const {
+					coords: { latitude, longitude },
+				} = location
+
+				fetch(
+					`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&sensor=true&key=AIzaSyBwJpOTDp1D6GOSGdGTATiCSN84gFEzuJE&language=pt-BR`
+				)
+					.then((res) => res.json())
+					.then((res) => {
+						const { formatted_address } = res.results[0]
+						setCurrentLocation(formatted_address)
+					})
+			}
+		})()
+	}, [])
+
 	return (
 		<View style={styles.container}>
-			<Text>DashBoard</Text>
-			<Button title="Sair" onPress={() => signOutUser()}>
-				Sign Out
-			</Button>
+			<Text>{currentLocation}</Text>
+			<Button title="Sair" onPress={() => signOutUser()} />
 		</View>
 	)
 }
